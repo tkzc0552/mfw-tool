2b02501dea039fcdecce476351c1cec963e10586
zhaohongming <hongming.zhao@seedeer.com>
2019-12-17 16:39:57 +0800

    add

---
+++ src/main/java/com/zhm/util/tool/Test.java
@@ -0,0 +1,18 @@
+package com.zhm.util.tool;
+
+
+import com.zhm.dto.Result;
+
+import java.util.Map;
+
+/**
+ * Created by 赵红明 on 2019/12/4.
+ */
+public class Test {
+
+    public static void main(String[] args){
+
+        System.out.println(DateUtil.getNowStringDate());
+
+    }
+}
