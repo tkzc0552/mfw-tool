344a89a88964662e1cd4115ccccc97b3ed3539a5
zhaohongming <hongming.zhao@seedeer.com>
2019-12-24 10:18:14 +0800

    添加参数

---
+++ src/main/java/com/zhm/jpa/essential/jpa/convert/AbstractEnumMapConverter.java
@@ -0,0 +1,26 @@
+package com.zhm.jpa.essential.jpa.convert;
+
+
+
+
+
+import com.zhm.util.tool.EnumMapUtils;
+
+import javax.persistence.AttributeConverter;
+import java.util.Map;
+
+public abstract class AbstractEnumMapConverter<E extends Enum> implements AttributeConverter<Map<E, Boolean>, Integer> {
+    protected Class<E> clazz;
+
+    @SuppressWarnings({"unchecked"})
+    @Override
+    public Integer convertToDatabaseColumn(Map<E, Boolean> attribute) {
+        return EnumMapUtils.toBinary(attribute);
+    }
+
+    @SuppressWarnings({"unchecked"})
+    @Override
+    public Map<E, Boolean> convertToEntityAttribute(Integer columnValue) {
+        return EnumMapUtils.toEnumMap(clazz, columnValue);
+    }
+}
---
+++ src/main/java/com/zhm/jpa/essential/jpa/search/BetweenType.java
@@ -0,0 +1,8 @@
+package com.zhm.jpa.essential.jpa.search;
+
+/**
+ * @author geewit
+ */
+public enum BetweenType {
+    LOW, HIGH
+}
